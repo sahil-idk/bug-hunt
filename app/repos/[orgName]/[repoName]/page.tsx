0abfbcb7d51dff7a21ad51691f0ea9c2bac44407
sahil-idk <sahilsahoo23@gmail.com>
2024-06-12 11:12:22 +0530

    typo fix

--- app/repos/[orgName]/[repoName]/page.tsx
+++ app/repos/[orgName]/[repoName]/page.tsx
@@ -1,54 +1,58 @@
-'use client'
-import { RepositoryDashboard } from '@/components/repository-dashboard'
-import { useQuery ,QueryCache} from '@tanstack/react-query'
-import React from 'react'
+"use client";
+import { RepositoryDashboard } from "@/components/repository-dashboard";
+import { useQuery, QueryCache } from "@tanstack/react-query";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const RepoPage = ({
-    params
-}:
-    {
-        params: {
-            orgName: string,
-            repoName: string
+  params,
+}: {
+  params: {
+    orgName: string;
+    repoName: string;
+  };
+}) => {
+  console.log(params.orgName, params.repoName);
+
+  const { data, isError, isLoading, error } = useQuery({
+    queryKey: ["repoDetails", params.orgName, params.repoName],
+    queryFn: () =>
+      fetch(
+        `https://api.github.com/repos/${params.orgName}/${params.repoName}`,
+        {
+          headers: {
+            Authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+          },
         }
-    }
-) => {
-    console.log(params.orgName, params.repoName);
-    
-      
-    const { data, isError, isLoading, error } = useQuery({
+      ).then((response) => response.json()),
+    refetchOnMount: true,
+  });
 
-        queryKey: ['repoDetails', params.orgName, params.repoName],
-        queryFn: () => fetch(`https://api.github.com/repos/${params.orgName}/${params.repoName}`, {
-            headers: {
-                Authorization: `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`
+  if (isLoading) {
+    return <span>Loading Repositry details </span>;
+  }
+  if (isError) {
+    return <span>Error loading repos: {error.message}</span>;
+  }
+  if (!data || data.length === 0) {
+    return <div>No repos available</div>;
+  }
 
-            }
-        }).then(response => response.json()),
-        refetchOnMount: true,
-    })
+  console.log(data);
 
+  return (
+    <div>
+      <RepositoryDashboard
+        orgName={params.orgName}
+        repoName={params.repoName}
+        watchers={data.watchers_count}
+        forks={data.forks_count}
+        description={data.description}
+        language={data.language}
+      />
+    </div>
+  );
+};
 
-    if (isLoading) {
-        return <span>Loading Repositry details  </span>;
-    }
-    if (isError) {
-        return <span>Error loading repos: {error.message}</span>;
-    }
-    if (!data || data.length === 0) {
-        return <div>No repos available</div>;
-    }
-
-    console.log(data)
-
-    return (
-        <div>
-           <h1 className='text-5xl'>{data.name}</h1>
-           <RepositoryDashboard/>
-        </div>
-    )
-}
-
-export default RepoPage
+export default RepoPage;
